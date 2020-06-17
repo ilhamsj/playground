2f1d08c425e14d3b324dc554a3db956da397dcc8
ilhamsj <ilhamsaputrajati@gmail.com>
2020-06-17 14:21:48 +0700

    crud

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/App.css";
+
+import Create from "./components/Create";
+import Edit from "./components/Edit";
+import Index from "./components/Index";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Link to={"/"}>Home</Link>
+      <Link to={"/index"}>index</Link>
+      <Link to={"/create"}>create</Link>
+      <Switch>
+        <Route exact path="/create" component={Create} />
+        <Route path="/index" component={Index} />
+        <Route path="/edit/:id" component={Edit} />
+      </Switch>
+    </Router>
   );
 }
 
